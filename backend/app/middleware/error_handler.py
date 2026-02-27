63fc05df60b50030782da8b0f2692b3f6f93ec45
ANKUR KUMAR <105548142+ankurkrr@users.noreply.github.com>
2026-02-27 18:27:49 +0530

    Fix import typo for JSONResponse in error_handler.py

--- backend/app/middleware/error_handler.py
+++ backend/app/middleware/error_handler.py
@@ -8,7 +8,7 @@ import logging
 
 from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponseeee
+from fastapi.responses import JSONResponseeeee
 
 from app.services.exceptions import AppException
 
